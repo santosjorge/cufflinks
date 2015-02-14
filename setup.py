82740968b82b4d6f4cfdec5dc4ac3501a583b97d
Jorge Santos <jorge.santos@thomsonreuters.com>
2015-02-14 19:16:45 +0000

    Modified Setup Files

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
 setup(name='cufflinks',
-      version='0.1',
+      version='0.2',
       description='Productivity Tools for Plotly + Pandas',
       author='Jorge Santos',
       author_email='santos.jorge@gmail.com',
       license='MIT',
+      keywords = ['pandas', 'plotly', 'plotting'],
+      url = 'https://github.com/santosjorge/cufflinks',
       packages=['cufflinks'],
-      zip_safe=False)
+	  zip_safe=False)
