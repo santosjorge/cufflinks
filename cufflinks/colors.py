82ecfea3cbb8677b882d6a590f4298b6c82f3dd0
Jorge Santos <jorge.santos@thomsonreuters.com>
2015-03-24 11:06:28 +0000

    Added color scales and color names

--- cufflinks/colors.py
+++ cufflinks/colors.py
@@ -1,10 +1,15 @@
 import collections
 import numpy as np
 import colorsys
+import colorlover as cl
+from utils import inverseDict
+import operator
 from IPython.display import HTML,display
 
 class CufflinksError(Exception):
 		pass
+
+
 
 # Colour Definitions
 # ---------------------------------
@@ -120,7 +125,7 @@ def normalize(color):
 			return '#'+''.join([x*2 for x in list(color)])
 	else:
 		try:
-			return normalize(eval(color))
+			return normalize(cnames[color.lower()])
 		except:
 			raise CufflinksError('Not a valid color')
 
@@ -210,13 +215,13 @@ def color_range(color,N=20):
 	hex_out.sort()
 	return hex_out
 
-def color_table(color,N=1):
+def color_table(color,N=1,sort=False,sort_values=False,inline=False):
 	"""
 	Generates a colour table 
 
 	Parameters:
 	-----------
-		color : string or list
+		color : string | list | dict
 			Color representation in rgba|rgb|hex
 			If a list of colors is passed then these
 			are displayed in a table
@@ -224,6 +229,11 @@ def color_table(color,N=1):
 			number of colours to generate
 			When color is not a list then it generaes 
 			a range of N colors
+		sort : bool 
+			if True then items are sorted
+		sort_values : bool 
+			if True then items are sorted by color values.
+			Only applies if color is a dictionary
 
 	Example:
 		color_range('#ff9933',20)
@@ -231,13 +241,24 @@ def color_table(color,N=1):
 	Note:
 		This function only works in iPython Notebook
 	"""		
-	if type(color)==list:
+	if isinstance(color,list):
 		c_=''
 		rgb_tup=[normalize(c) for c in color]
+		if sort:
+			rgb_tup.sort()
+	elif isinstance(color,dict):
+		c_=''
+		items=[(k,normalize(v),hex_to_hsv(normalize(v))) for k,v in color.items()]
+		if sort:
+			if sort_values:
+				items=sorted(items,key=operator.itemgetter(2))
+			else:
+				items=sorted(items,key=operator.itemgetter(0))
+		rgb_tup=[(k,v) for k,v,_ in items]
 	else:
 		c_=normalize(color)
 		if N>1:
-			rgb_tup=np.array(color_range(c_,N))
+			rgb_tup=np.array(color_range(c_,N))[::-1]
 		else:
 			rgb_tup=[c_]
 	def _color(c):
@@ -252,15 +273,25 @@ def color_table(color,N=1):
 		else:
 			border='' 
 		return color,shadow,border
-
-	s='<ul style="list-style-type: none;">'
-	for c in reversed(rgb_tup):
-		color,shadow,border=_color(c)
-		s+="""<li style="text-align:center;"""+border+"""line-height:30px;background-color:"""+ c + """;"> 
-		<span style=" text-shadow:"""+shadow+"""; color:"""+color+""";">"""+c+"""</span>
-		</li>"""
-	s+='</ul>'
+	
+	s='<ul style="list-style-type: none;">' if not inline else ''
+	for c in rgb_tup:
+		if isinstance(c,tuple):
+			k,c=c
+			k+=' : '
+		else:
+			k=''
+		if inline:
+			s+='<div style="background-color:{0};height:20px;width:20px;display:inline-block;"></div>'.format(c)    
+		else:
+			color,shadow,border=_color(c)
+			s+="""<li style="text-align:center;"""+border+"""line-height:30px;background-color:"""+ c + """;"> 
+			<span style=" text-shadow:"""+shadow+"""; color:"""+color+""";">"""+k+c.upper()+"""</span>
+			</li>"""
+	s+='</ul>' if not inline else ''
 	return HTML(s)
+
+
 
 
 def colorgen(colors=None):
@@ -286,3 +317,235 @@ def colorgen(colors=None):
 		for y in dq:
 			yield to_rgba(y,1-i)
 		dq.rotate()
+
+# NEW STUFF
+
+_scales=None
+_scales_titles=None
+
+def interp(colors,N):
+	def _interp(colors,N):
+		try:
+			return cl.interp(colors,N)
+		except:
+			return _interp(colors,N+1)
+	c=_interp(colors,N)
+	return map(rgb_to_hex,cl.to_rgb(c))
+
+def scales(key='dflt'):
+	display(HTML(cl.to_html(_scales)))
+
+# Scales Dictionary
+# ---------------------------------
+
+def reset_scales():
+	global _scales
+	global _scales_titles
+	scale_cpy=cl.scales.copy()
+	_scales={}
+	for k,v in scale_cpy.items():
+		for k_,v_ in v.items():
+			if k_ not in _scales:
+				_scales[k_]={}
+			for k__,v__ in v_.items():
+				if k__ not in _scales[k_]:
+					_scales[k_][k__]={}
+				_scales[k_][k__][k]=v__
+
+	_scales_titles={}
+	for k,v in scale_cpy.items():
+		for k_,v_ in v.items():
+			for k__,v__ in v_.items():
+				k__=k__.lower()
+				if k__ not in _scales_titles:
+					_scales_titles[k__]={}
+				_scales_titles[k__][k]=v__            
+
+	# dflt=[normalize(v) for v in ['orange','blue','grassgreen','purple',
+	# 											'red','teal','yellow','olive','salmon','lightblue2']]
+	# _scales_titles['dflt']={str(len(dflt)):dflt}
+
+reset_scales()
+
+# Color Names
+# ---------------------------------
+
+
+cnames={'aliceblue': '#F0F8FF',
+ 'antiquewhite':	 '#FAEBD7',
+ 'aqua':			 '#00FFFF',
+ 'aquamarine':		 '#7FFFD4',
+ 'azure':			 '#F0FFFF',
+ 'beige':			 '#F5F5DC',
+ 'bisque':			 '#FFE4C4',
+ 'black':			 '#000000',
+ 'blanchedalmond':	 '#FFEBCD',
+ 'blue':			 '#3780bf',
+ 'bluepurple':		 '#6432AB',
+ 'blueviolet':		 '#8A2BE2',
+ 'brightblue':		 '#0000FF',
+ 'brightred':		 '#FF0000',
+ 'brown':			 '#A52A2A',
+ 'burlywood':		 '#DEB887',
+ 'cadetblue':		 '#5F9EA0',
+ 'charcoal':		 '#151516',
+ 'chartreuse':		 '#7FFF00',
+ 'chocolate':		 '#D2691E',
+ 'coral':			 '#FF7F50',
+ 'cornflowerblue':	 '#6495ED',
+ 'cornsilk':		 '#FFF8DC',
+ 'crimson':			 '#DC143C',
+ 'cyan':			 '#00FFFF',
+ 'darkblue':		 '#00008B',
+ 'darkcyan':		 '#008B8B',
+ 'darkgoldenrod':	 '#B8860B',
+ 'darkgray':		 '#A9A9A9',
+ 'darkgreen':		 '#006400',
+ 'darkgrey':		 '#A9A9A9',
+ 'darkkhaki':		 '#BDB76B',
+ 'darkmagenta':		 '#8B008B',
+ 'darkolivegreen':	 '#556B2F',
+ 'darkorange':		 '#FF8C00',
+ 'darkorchid':		 '#9932CC',
+ 'darkred':			 '#8B0000',
+ 'darksalmon':		 '#E9967A',
+ 'darkseagreen':	 '#8FBC8F',
+ 'darkslateblue':	 '#483D8B',
+ 'darkslategray':	 '#2F4F4F',
+ 'darkslategrey':	 '#2F4F4F',
+ 'darkturquoise':	 '#00CED1',
+ 'darkviolet':		 '#9400D3',
+ 'deeppink':		 '#FF1493',
+ 'deepskyblue':		 '#00BFFF',
+ 'dimgray':			 '#696969',
+ 'dimgrey':			 '#696969',
+ 'dodgerblue':		 '#1E90FF',
+ 'firebrick':		 '#B22222',
+ 'floralwhite':		 '#FFFAF0',
+ 'forestgreen':		 '#228B22',
+ 'fuchsia':			 '#FF00FF',
+ 'gainsboro':		 '#DCDCDC',
+ 'ghostwhite':		 '#F8F8FF',
+ 'gold':			 '#FFD700',
+ 'goldenrod':		 '#DAA520',
+ 'grassgreen':		 '#32ab60',
+ 'gray':			 '#808080',
+ 'green':			 '#008000',
+ 'greenyellow':		 '#ADFF2F',
+ 'grey':			 '#808080',
+ 'grey01':			 '#0A0A0A',
+ 'grey02':			 '#151516',
+ 'grey03':			 '#1A1A1C',
+ 'grey04':			 '#1E1E21',
+ 'grey05':			 '#252529',
+ 'grey06':			 '#36363C',
+ 'grey07':			 '#3C3C42',
+ 'grey08':			 '#434343',
+ 'grey09':			 '#666570',
+ 'grey10':			 '#666666',
+ 'grey11':			 '#8C8C8C',
+ 'grey12':			 '#C2C2C2',
+ 'grey13':			 '#E2E2E2',
+ 'honeydew':		 '#F0FFF0',
+ 'hotpink':			 '#FF69B4',
+ 'indianred':		 '#CD5C5C',
+ 'indigo':			 '#4B0082',
+ 'ivory':			 '#FFFFF0',
+ 'khaki':			 '#F0E68C',
+ 'lavender':		 '#E6E6FA',
+ 'lavenderblush':	 '#FFF0F5',
+ 'lawngreen':		 '#7CFC00',
+ 'lemonchiffon':	 '#FFFACD',
+ 'lightpink2':		 '#fccde5',
+ 'lightpurple':		 '#bc80bd',
+ 'lightblue':		 '#ADD8E6',
+ 'lightcoral':		 '#F08080',
+ 'lightcyan':		 '#E0FFFF',
+ 'lightgoldenrodyellow':	 '#FAFAD2',
+ 'lightgray':		 '#D3D3D3',
+ 'lightgreen':		 '#90EE90',
+ 'lightgrey':		 '#D3D3D3',
+ 'lightpink':		 '#FFB6C1',
+ 'lightsalmon':		 '#FFA07A',
+ 'lightseagreen':	 '#20B2AA',
+ 'lightskyblue':	 '#87CEFA',
+ 'lightslategray':	 '#778899',
+ 'lightslategrey':	 '#778899',
+ 'lightsteelblue':	 '#B0C4DE',
+ 'lightteal':		 '#8dd3c7',
+ 'lightyellow':		 '#FFFFE0',
+ 'lightblue2':		 '#80b1d3',
+ 'lime':			 '#00FF00',
+ 'limegreen':		 '#32CD32',
+ 'linen':			 '#FAF0E6',
+ 'magenta':			 '#FF00FF',
+ 'maroon':			 '#800000',
+ 'mediumaquamarine': '#66CDAA',
+ 'mediumblue':		 '#0000CD',
+ 'mediumorchid':	 '#BA55D3',
+ 'mediumpurple':	 '#9370DB',
+ 'mediumseagreen':	 '#3CB371',
+ 'mediumslateblue':	 '#7B68EE',
+ 'mediumspringgreen':'#00FA9A',
+ 'mediumturquoise':	 '#48D1CC',
+ 'mediumvioletred':	 '#C71585',
+ 'midnightblue':	 '#191970',
+ 'mintcream':		 '#F5FFFA',
+ 'mistyrose':		 '#FFE4E1',
+ 'moccasin':		 '#FFE4B5',
+ 'navajowhite':		 '#FFDEAD',
+ 'navy':			 '#000080',
+ 'oldlace':			 '#FDF5E6',
+ 'olive':			 '#808000',
+ 'olivedrab':		 '#6B8E23',
+ 'orange':			 '#ff9933',
+ 'orangered':		 '#FF4500',
+ 'orchid':			 '#DA70D6',
+ 'palegoldenrod':	 '#EEE8AA',
+ 'palegreen':		 '#98FB98',
+ 'paleolive':		 '#b3de69',
+ 'paleturquoise':	 '#AFEEEE',
+ 'palevioletred':	 '#DB7093',
+ 'papayawhip':		 '#FFEFD5',
+ 'peachpuff':		 '#FFDAB9',
+ 'pearl':			 '#D9D9D9',
+ 'pearl02':			 '#F5F6F9',
+ 'pearl03':			 '#E1E5ED',
+ 'pearl04':			 '#9499A3',
+ 'pearl05':			 '#6F7B8B',
+ 'pearl06':			 '#4D5663',
+ 'peru':			 '#CD853F',
+ 'pink':			 '#ff0088',
+ 'plum':			 '#DDA0DD',
+ 'powderblue':		 '#B0E0E6',
+ 'purple':			 '#800080',
+ 'red':				 '#db4052',
+ 'rose':			 '#FFC0CB',
+ 'rosybrown':		 '#BC8F8F',
+ 'royalblue':		 '#4169E1',
+ 'saddlebrown':		 '#8B4513',
+ 'salmon':			 '#fb8072',
+ 'sandybrown':		 '#FAA460',
+ 'seaborn':			 '#EAE7E4',
+ 'seagreen':		 '#2E8B57',
+ 'seashell':		 '#FFF5EE',
+ 'sienna':			 '#A0522D',
+ 'silver':			 '#C0C0C0',
+ 'skyblue':			 '#87CEEB',
+ 'slateblue':		 '#6A5ACD',
+ 'slategray':		 '#708090',
+ 'slategrey':		 '#708090',
+ 'snow':			 '#FFFAFA',
+ 'springgreen':		 '#00FF7F',
+ 'steelblue':		 '#4682B4',
+ 'tan':				 '#D2B48C',
+ 'teal':			 '#008080',
+ 'thistle':			 '#D8BFD8',
+ 'tomato':			 '#FF6347',
+ 'turquoise':		 '#40E0D0',
+ 'violet':			 '#EE82EE',
+ 'wheat':			 '#F5DEB3',
+ 'white':			 '#FFFFFF',
+ 'whitesmoke':		 '#F5F5F5',
+ 'yellow':			 '#ffff33',
+ 'yellowgreen':		 '#9ACD32'}		
