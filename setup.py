cf38f0d543e74a2f1aaff5c329dd1102a295fe27
Jorge Santos <jorgesantos@jorgesmptouchid.ten.thomsonreuters.com>
2017-11-07 15:11:34 +0000

    Fixes #941

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cufflinks',
-      version='0.12.0',
+      version='0.12.1',
       description='Productivity Tools for Plotly + Pandas',
       author='Jorge Santos',
       author_email='santos.jorge@gmail.com',
