bea718f2e54f8db925f02ef68c027c0c6a6a3de5
Guillaume <jeusel.guillaume@gmail.com>
2018-04-18 21:47:18 +0200

    Call init_notebook_mode only if inside IPython

--- cufflinks/offline.py
+++ cufflinks/offline.py
@@ -3,13 +3,22 @@ import plotly.offline as py_offline
 # Offline Mode
 
 
-def go_offline(connected=False):
+def run_from_ipython():
     try:
-        py_offline.init_notebook_mode(connected)
-    except TypeError:
-        # For older versions of plotly
-        py_offline.init_notebook_mode()
-    py_offline.__PLOTLY_OFFLINE_INITIALIZED = True
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
+
+def go_offline(connected=False):
+    if run_from_ipython():
+        try:
+            py_offline.init_notebook_mode(connected)
+        except TypeError:
+            #For older versions of plotly
+            py_offline.init_notebook_mode()
+        py_offline.__PLOTLY_OFFLINE_INITIALIZED=True
 
 
 def go_online():
